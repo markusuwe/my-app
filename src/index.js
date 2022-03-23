6e86300c809d26da9d902e0d5474105413a54e51
Markus Mueller <markus.mueller@bayanat.ai>
2022-03-23 10:14:37 +0400

    Use arrow function syntax

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={function () { console.log('click'); }}>
+      <button className="square" onClick={()=> console.log('clickclick')}>
         {this.props.value}
       </button>
     );
