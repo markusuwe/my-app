11d5eae56484c4d40fe99771439d7dd9645e28f2
Markus Mueller <markus.mueller@bayanat.ai>
2022-03-24 13:58:36 +0400

    Implement taking turns

--- src/index.js
+++ src/index.js
@@ -15,14 +15,18 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
-        }
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -33,10 +37,10 @@ class Board extends React.Component {
     );
   }
 
-  render() {
-    const status = 'Next player: X';
-
-    return (
+    render() {
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        
+        return (
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
